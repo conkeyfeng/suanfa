f1258651899a6815640bc3710d23a0caedc3cde6
domofeng <chenyang.feng.cn@gmail.com>
2019-04-30 09:31:59 -0700

    1

--- LC/src/Solution.java
+++ LC/src/Solution.java
@@ -1,3 +1,111 @@
-public class Solution {
+import java.util.*;
 
+public class Solution {
+    static int MOD = 1_000_000_007;
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String ss = "[]";
+        System.out.println(ss.length());
+        int[] arr = new int[]{0,1,0};
+        Interval a = new Interval(0,30);
+        Interval b = new Interval(5,10);
+        Interval c = new Interval(15,20);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/**
+ *  BELOW ARE THE LIBRARY
+ */
+
+
+class Point{
+    public int x,y,val;
+    public Point(int x, int y, int val){
+        this.x = x;
+        this.y = y;
+        this.val = val;
+    }
+}
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+
+class TrieNode {
+    TrieNode[] children;
+    boolean isEndOfWord;
+
+    TrieNode() {
+        isEndOfWord = false;
+        children = new TrieNode[26];
+    }
+
+    public void insert(String key) {
+        int index;
+        TrieNode pCrawl = this;
+
+        for (int i = 0; i < key.length(); i++) {
+            index = key.charAt(i) - 'a';
+            if (pCrawl.children[index] == null)
+                pCrawl.children[index] = new TrieNode();
+
+            pCrawl = pCrawl.children[index];
+        }
+        pCrawl.isEndOfWord = true;
+    }
+
+    public boolean search(String key) {
+        int index;
+        TrieNode pCrawl = this;
+
+        for (int i = 0; i < key.length(); i++) {
+            index = key.charAt(i) - 'a';
+            if (pCrawl.children[index] == null)
+                return false;
+            pCrawl = pCrawl.children[index];
+        }
+        return (pCrawl != null && pCrawl.isEndOfWord);
+    }
+
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Interval {
+    int start;
+    int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
 }
